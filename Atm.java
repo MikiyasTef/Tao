8600ce08d4a893f14b5ce62a02df36405f35e4d2
MikiyasTef <mikiyastef@gmail.com>
2019-05-20 01:37:12 -0700

    Fi

---
+++ Atm.java
@@ -0,0 +1,29 @@
+package System;
+
+
+/**
+ * @author MikeE
+ * @version 1.0
+ * @created 20-May-2019 12:47:35 AM
+ */
+class Atm (){
+
+	public int ExpiryDate;
+	public char Location;
+	private int Managed by;
+	public Customer m_Customer;
+	public withdrawal transaction m_withdrawal transaction;
+
+	public Atm(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	private double Transfer_money(){
+		return 0;
+	}
+
+}
